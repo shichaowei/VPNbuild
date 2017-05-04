1988420674ded450e2263d08c5cfd8393c623188
shichaowei <1239378293@qq.com>
2017-05-04 20:53:25 +0800

    made a change for vpnbuild

--- vpnbulid.sh
+++ vpnbulid.sh
@@ -2,7 +2,7 @@ sed -i "s/#localip 192.168.0.1/localip 192.168.0.1/g" /etc/pptpd.conf;
 sed -i "s/#remoteip 192.168.0.234-238,192.168.0.245/remoteip 192.168.0.234-238,192.168.0.245/g" /etc/pptpd.conf;
 sed -i "s/#ms-dns 10.0.0.1/ms-dns 8.8.8.8/g" /etc/ppp/pptpd-options;
 sed -i "s/#ms-dns 10.0.0.2/ms-dns 8.8.4.4/g" /etc/ppp/pptpd-options;
-sed -i "/IP addresses/a cc pptpd 123456 * " /etc/ppp/chap-secrets;
+sed -i "/IP addresses/a dd pptpd 123456 * " /etc/ppp/chap-secrets;
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf;
 sysctl -p;
 iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -o eth0 -j MASQUERADE;
